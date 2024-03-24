1a7d98a7f9c63803ebfba12d7f75f4277a49e55a
aimrane <aimrane.essakhi@gmail.com>
2024-03-24 13:50:13 +0000

    create DTOs (update and create adtConstRequest)

---
+++ notification-service/src/main/java/com/adria/notification/dto/request/adtConsts/SaveADTConstRequestDTO.java
@@ -0,0 +1,16 @@
+package com.adria.notification.dto.request.adtConsts;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SaveADTConstRequestDTO {
+
+    private String code;
+
+    private String value;
+
+}
---
+++ notification-service/src/main/java/com/adria/notification/dto/request/adtConsts/UpdateADTConstRequestDTO.java
@@ -0,0 +1,20 @@
+package com.adria.notification.dto.request.adtConsts;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateADTConstRequestDTO {
+
+    @NotEmpty
+    private String id;
+
+    @NotEmpty
+    private String value;
+
+}
