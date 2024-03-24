e3bbec8021fe7d61d34faf681f52de87abac5bc6
aimrane <aimrane.essakhi@gmail.com>
2024-03-24 13:51:48 +0000

    create DTOs (response ADTConstCodeResponseDTO and ADTConstResponseDTO)

---
+++ notification-service/src/main/java/com/adria/notification/dto/response/adtConsts/ADTConstResponseDTO.java
@@ -0,0 +1,18 @@
+package com.adria.notification.dto.response.adtConsts;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ADTConstResponseDTO {
+    private UUID id;
+
+    private String code;
+
+    private String value;
+}
