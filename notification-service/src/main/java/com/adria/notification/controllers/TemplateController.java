8e21558f9411dcce3100ae7b4b16a8a9db1673cb
aimrane <aimrane.essakhi@gmail.com>
2024-04-24 15:33:33 +0100

    Create Template controller and mapper

---
+++ notification-service/src/main/java/com/adria/notification/controllers/TemplateController.java
@@ -0,0 +1,61 @@
+package com.adria.notification.controllers;
+
+import com.adria.notification.dto.request.template.EmailTemplateRequestDto;
+import com.adria.notification.dto.request.template.SmsTemplateRequestDto;
+import com.adria.notification.dto.response.template.EmailTemplateResponseDto;
+import com.adria.notification.dto.response.template.SaveEmailTemplateResponseDto;
+import com.adria.notification.dto.response.template.SmsTemplateResponseDto;
+import com.adria.notification.services.ITemplateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/templates")
+public class TemplateController {
+    final private ITemplateService templateService;
+
+    @PostMapping("/saveEmail")
+    public ResponseEntity<EmailTemplateRequestDto> saveEmail(@Valid @RequestBody EmailTemplateRequestDto emailTemplate){
+        return ResponseEntity.ok().body(templateService.saveEmail(emailTemplate));
+    }
+
+    @PostMapping("/updateEmail")
+    public ResponseEntity<EmailTemplateRequestDto> updateEmail(@Valid @RequestBody EmailTemplateRequestDto emailTemplate){
+        return ResponseEntity.ok().body(templateService.updateEmail(emailTemplate));
+    }
+
+    @PostMapping("/deleteEmail")
+    public void deleteEmail(@Valid @RequestBody EmailTemplateResponseDto emailTemplate){
+        templateService.deleteEmail(emailTemplate);
+    }
+
+    @PostMapping("/saveSms")
+    public ResponseEntity<SmsTemplateRequestDto> saveSms(@Valid @RequestBody SmsTemplateRequestDto smsTemplate){
+        return ResponseEntity.ok().body(templateService.saveSms(smsTemplate));
+    }
+
+    @PostMapping("/updateSms")
+    public ResponseEntity<SmsTemplateRequestDto> updateSms(@Valid @RequestBody SmsTemplateRequestDto smsTemplate){
+        return ResponseEntity.ok().body(templateService.updateSms(smsTemplate));
+    }
+
+    @PostMapping("/deleteSms")
+    public void deleteSms(@Valid @RequestBody SmsTemplateResponseDto smsTemplate){
+        templateService.deleteSms(smsTemplate);
+    }
+
+    @GetMapping("/findAllEmail")
+    public ResponseEntity<List<EmailTemplateResponseDto>> findAllEmail(@RequestParam String type){
+        return ResponseEntity.ok().body(templateService.findAllEmail(type));
+    }
+
+    @GetMapping("/findAllSms")
+    public ResponseEntity<List<SmsTemplateResponseDto>> findAllSms(@RequestParam String type){
+        return ResponseEntity.ok().body(templateService.findAllSms(type));
+    }
+}
---
+++ notification-service/src/main/java/com/adria/notification/mappers/TemplateMapper.java
@@ -0,0 +1,38 @@
+package com.adria.notification.mappers;
+
+import com.adria.notification.dto.request.template.EmailTemplateRequestDto;
+import com.adria.notification.dto.request.template.SmsTemplateRequestDto;
+import com.adria.notification.dto.response.preferences.SavePreferencesResponseDto;
+import com.adria.notification.dto.response.template.EmailTemplateResponseDto;
+import com.adria.notification.dto.response.template.SaveEmailTemplateResponseDto;
+import com.adria.notification.dto.response.template.SmsTemplateResponseDto;
+import com.adria.notification.models.entities.Preferences;
+import com.adria.notification.models.entities.Template;
+import org.mapstruct.Builder;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(builder = @Builder(disableBuilder = true), componentModel = "spring")
+public interface TemplateMapper {
+    @Mapping(target = "event.id", source = "event.id")
+    EmailTemplateRequestDto toEmailTemplateDto(Template template);
+    @Mapping(target = "eventName", source = "event.name")
+    EmailTemplateResponseDto toEmailResponseTemplateDto(Template template);
+    @Mapping(target = "eventName", source = "event.name")
+    SmsTemplateResponseDto toSmsResponseTemplateDto(Template template);
+    @Mapping(source = "event.id", target = "event.id")
+    Template toEmailTemplateEntity(EmailTemplateRequestDto templateDto);
+    @Mapping(source = "eventName", target = "event.name")
+    Template toEmailTemplateEntity(EmailTemplateResponseDto templateDto);
+    @Mapping(target = "event.id", source = "event.id")
+    SmsTemplateRequestDto toSmsTemplateDto(Template template);
+    @Mapping(source = "event.id", target = "event.id")
+    Template toSmsTemplateEntity(SmsTemplateRequestDto templateDto);
+    @Mapping(source = "eventName", target = "event.name")
+    Template toSmsTemplateEntity(SmsTemplateResponseDto templateDto);
+    List<EmailTemplateResponseDto> toEmailTemplateDtoList(List<Template> templates);
+    List<SmsTemplateResponseDto> toSmsTemplateDtoList(List<Template> templates);
+
+}
