91d59a9423ec5f28d6551f240acb00b458c2e462
aimrane <aimrane.essakhi@gmail.com>
2024-04-29 04:02:54 +0100

    update EmailProvider dtos

---
+++ notification-service/src/main/java/com/adria/notification/dto/request/emailProvider/SaveEmailProviderRequestDTO.java
@@ -0,0 +1,30 @@
+package com.adria.notification.dto.request.emailProvider;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SaveEmailProviderRequestDTO {
+
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private String mailHost;
+    @NotEmpty
+    private String mailPort;
+    @NotEmpty
+    private String mailUsername;
+    @NotEmpty
+    private String mailPassword;
+    @NotEmpty
+    private String mailProtocol;
+    private boolean smtpAuth;
+    private boolean starttlsEnable;
+    private String sslTrust;
+
+}
