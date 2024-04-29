98f74502abed68ec6d47effcaba2561f53580643
aimrane <aimrane.essakhi@gmail.com>
2024-04-29 04:01:28 +0100

    Create EmailProvider classe and repository

---
+++ notification-service/src/main/java/com/adria/notification/models/entities/EmailProvider.java
@@ -0,0 +1,39 @@
+package com.adria.notification.models.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class EmailProvider extends AbstractEntity {
+    @Column(unique = true, nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String mailHost;
+    @Column(nullable = false)
+    private String mailPort;
+    @Column(nullable = false)
+    private String mailUsername;
+    @Column(nullable = false)
+    private String mailPassword;
+    @Column(nullable = false)
+    private String mailProtocol;
+    @Column(nullable = false)
+    @ColumnDefault("'0'")
+    @Type(type = "org.hibernate.type.NumericBooleanType")
+    private boolean smtpAuth;
+    @Column(nullable = false)
+    @ColumnDefault("'0'")
+    @Type(type = "org.hibernate.type.NumericBooleanType")
+    private boolean starttlsEnable;
+    @Column(nullable = false)
+    private String sslTrust;
+}
