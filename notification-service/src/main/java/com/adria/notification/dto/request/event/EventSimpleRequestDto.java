0458a79656db5f8ef7f91c955ee44f2e4521e449
aimrane <aimrane.essakhi@gmail.com>
2024-05-08 10:46:01 +0100

    add a event dto for references

---
+++ notification-service/src/main/java/com/adria/notification/dto/request/event/EventSimpleRequestDto.java
@@ -0,0 +1,14 @@
+package com.adria.notification.dto.request.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EventSimpleRequestDto {
+    private String eventName;
+    private boolean editable;
+    private String notificationType;
+}
