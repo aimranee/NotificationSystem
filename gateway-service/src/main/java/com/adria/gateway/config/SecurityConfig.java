9cecbf7af76714fa0244b83bb7c476d7c99eff0f
aimrane <aimrane.essakhi@gmail.com>
2024-05-28 18:04:20 +0100

    add required configuration to secure gateway with keycloak

--- gateway-service/src/main/java/com/adria/gateway/config/SecurityConfig.java
+++ gateway-service/src/main/java/com/adria/gateway/config/SecurityConfig.java
@@ -2,8 +2,11 @@ package com.adria.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -11,20 +14,21 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) throws Exception {
-//        serverHttpSecurity.csrf()
-//                .disable()
-//                .authorizeExchange(exchange -> exchange
-//                        .pathMatchers("/urlshortening-service/**")
-//                        .permitAll()
-//                        .anyExchange()
-//                        .authenticated())
-//                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
-//        return serverHttpSecurity.build();
         return serverHttpSecurity
-                .authorizeExchange().pathMatchers("/actuator/health/**").permitAll()
+                .csrf().disable()
+                .cors().and()
+                .authorizeExchange().pathMatchers("/actuator/health/**", "/login", "/refresh-token").permitAll()
+                .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .anyExchange().authenticated()
                 .and()
-                .oauth2ResourceServer().jwt().and()
-                .and().build();
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())))
+                .build();
     }
+
+    private ReactiveJwtAuthenticationConverterAdapter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
+    }
+
 }
